766c9a218cf02adb4a68202215a1ff7236ef8f94
USER <USER@LAPTOP-8VR68G1P>
2018-11-06 15:10:03 +0800

    Added main menu

--- src/asd/ASD.java
+++ src/asd/ASD.java
@@ -5,6 +5,8 @@
  */
 package asd;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USER
@@ -15,7 +17,82 @@ public class ASD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        mainMenu();
+    }
+    
+    public static void mainMenu(){
+        Scanner sc = new Scanner(System.in);
+        boolean invalidInput = true;
+        
+        System.out.println("Fiore Flowershop");
+        System.out.println("================");
+        System.out.println("1. Catalog Maintenance");
+        System.out.println("2. Customer Maintenance and Invoice Payment");
+        System.out.println("3. Catalog Order");
+        System.out.println("4. Order Pickup/Delivery and Consumer Payment Management");
+        System.out.println("\n0. Exit");
+        
+        while(invalidInput){
+            System.out.print("\nPlease select a choice: ");
+            String selection = sc.next();
+            sc.nextLine();
+            
+            if (selection.equals("1")){
+                invalidInput=false;
+                module1();
+            }
+            else if(selection.equals("2")){
+                invalidInput=false;
+            }
+            else if (selection.equals("3")){
+                invalidInput=false;
+            }
+            else if (selection.equals("4")){
+                invalidInput=false;
+            }
+            else if (selection.equals("0")){
+                invalidInput=false;
+                System.exit(0);
+            }
+            else{
+                System.out.println("Invalid Input. Please enter only 1, 2, 3, 4 or 0");
+            }
+        }
+    }
+    public static void module1(){
+        Scanner sc = new Scanner(System.in);
+        boolean invalidInput = true;
+        
+        System.out.println("Product Type");
+        System.out.println("============");
+        System.out.println("1. Fresh Flower");
+        System.out.println("2. Bouquet");
+        System.out.println("3. Floral Arrangement");
+        System.out.println("\n0. Back");
+        
+        while(invalidInput){
+            System.out.print("\nPlease select a choice: ");
+            String selection = sc.next();
+            sc.nextLine();
+            
+            if (selection.equals("1")){
+                invalidInput=false;
+                
+            }
+            else if(selection.equals("2")){
+                invalidInput=false;
+            }
+            else if (selection.equals("3")){
+                invalidInput=false;
+            }
+            else if (selection.equals("0")){
+                invalidInput=false;
+                mainMenu();
+            }
+            else{
+                System.out.println("Invalid Input. Please enter only 1, 2, 3 or 0");
+            }
+        }
     }
     
 }
